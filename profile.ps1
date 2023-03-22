23bf89f183f0d2adcf1e8d0552c20a7859d850fb
Dipaditya Das <DipadityaDas@gmail.com>
2023-03-22 23:51:31 +0530

    Create for Custom profile
    Create for Custom profile for PowerShell Core 7.3.3

--- profile.ps1
+++ profile.ps1
@@ -1,23 +1,41 @@
-Import-Module oh-my-posh
-Import-Module posh-git
+# Author: Dipaditya Das
+# https://github.com/DipadityaDas
+# https://www.linkedin.com/in/dipadityadas/
 
-Set-PoshPrompt -Theme Dipadityadas
+# Aliases
+Set-Alias tt tree
+Set-Alias ll ls
+Set-Alias gg gcc
 
+# Prompt
+oh-my-posh init pwsh --config "C:\Program Files\PowerShell\theme\dax.omp.json" | Invoke-Expression
+
+# Changing Colors of Input provided to Powershell prompt
 Set-PSReadLineOption -Colors @{
-  Command            = 'Cyan'
-  Number             = 'Yellow'
+  Command            = 'Cyan'    # Executable file
+  Default            = 'Yellow'  # Anything other than command
+  Number             = 'blue'    # 108 is a number
   Member             = 'Yellow'
-  Operator           = 'Green'
-  Type               = 'Green'
-  Variable           = 'Green'
-  Parameter          = 'Green'
-  ContinuationPrompt = 'Cyan'
-  Default            = 'Yellow'
+  Operator           = 'Green'   # 6*6 -> * is the operator
+  Type               = 'Green'   # [int]"108" -> int is the type
+  Parameter          = 'Green'   # -s --something
+  Variable           = 'Blue'    # @something
+  String             = 'Magenta' # "108" 
+  ContinuationPrompt = 'Cyan'    # Multiline Command starting with '> '
 }
 
-# Shows navigable menu of all options when hitting Tab
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+# (Get-PSReadLineOption).ContinuationPrompt -> '> ' (Default)
+# Changing the ContinutionPrompt Character
+Set-PSReadLineOption -ContinuationPrompt '> '
 
-# Autocompletion for arrow keys
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+# Create Predictions using History
+set-PSReadLineOption -PredictionSource History
+set-PSReadLineOption -PredictionViewStyle ListView
+
+# Add Z as an smart alternative of cd
+Import-Module -Name z 
+
+# Adding Terminal Icons
+# Import-Module -Name Terminal-Icons 
+
+
